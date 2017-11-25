7927c396dde7650268388a132c687369b6cea0ad
Rushat Gabhane <rushatgabhane@gmail.com>
2017-11-25 15:01:13 +0400

    Add files via upload

---
+++ physics.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import spline
+
+# x axis values
+X = np.array([30, 35, 45, 50, 55, 60])
+# y axis values
+Y = np.array([26, 25, 24, 25, 26, 27])
+
+# Greater the smooth factor, smoother the curve and vice-versa
+smooth_factor = 250
+x = np.linspace(X.min(), X.max(), smooth_factor)
+
+# Smmooothens the curve using spline
+smooth = spline(X, Y, x)
+plt.plot(x, smooth)
+plt.tight_layout()
+plt.show()
